8b4283d4409cb9741cf51c92d84e36cf0fbb6d05
uweradiane <uweradiane27@gmail.com>
2023-11-13 12:36:38 -0500

    enregistrement et connection

---
+++ connection.php
@@ -0,0 +1,35 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ecom1_tp2";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Process form submission
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $street = $_POST['street'];
+    $street_nb= $_POST['street_nb'];
+    $typ = $_POST['type'];
+    $city = $_POST['city'];
+    $zipcode = $_POST['zipcode'];
+
+    // Insert data into the database
+    $sql =("INSERT INTO adress(street, street_nb, type, city,zipcode) VALUES ('$street', '$street_nb', '$typ', '$city', '$zipcode')");
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Item added successfully to the database";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+// Close the connection
+$conn->close();
+?> 
---
+++ enregistrement.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Adresses</title>
+</head>
+<body>
+    <form action="connection.php" method="post">
+        <?php 
+            //$num_addresses=1;
+            $num_addresses = $_POST['num_addresses'];
+            for ($i = 1; $i <= $num_addresses; $i++): ?>
+            <h2>Adresse  <?php echo $i; ?></h2>
+            <label for="street">Street:</label>
+            <input type="text" name="street">
+
+            <label for="street_nb<">Street Number:</label>
+            <input type="number" name="street_nb">
+
+            <label for="type">Type:</label>
+            <select name="type" id="typ">
+                <option value="livraison">Livraison</option>
+                <option value="facturation">Facturation</option>
+                <option value="autre">Autre</option>
+            </select>
+
+            <label for="city">City:</label>
+            <select name="city">
+                <option value="Montreal">Montreal</option>
+                <option value="Laval">Laval</option>
+                <option value="Toronto">Toronto</option>
+                <option value="Quebec">Quebec</option>
+            </select>
+
+            <label for="zipcode">zipcode:</label>
+            <input type="text" name="zipcode">
+        <?php endfor;?>
+
+        <button type="submit">Soumetre</button>
+    </form>
+</body>
+</html>
